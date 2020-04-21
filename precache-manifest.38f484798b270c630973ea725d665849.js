bc1595f872b0cf96005a89068e148ab6ea1e4878
WillC <wenweicui@gmail.com>
2020-04-21 15:51:30 -0400

    Updates

---
+++ precache-manifest.38f484798b270c630973ea725d665849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3feb8d660f37d4566840473a5c642a82",
+    "url": "/react-covid-19/index.html"
+  },
+  {
+    "revision": "7093bfc53d226cfd700c",
+    "url": "/react-covid-19/static/css/main.11cb7078.chunk.css"
+  },
+  {
+    "revision": "877cfaf72fac0a6e9c8b",
+    "url": "/react-covid-19/static/js/2.a5bc2c42.chunk.js"
+  },
+  {
+    "revision": "0f7d5ba1d9311badd66e131e11c5bbc2",
+    "url": "/react-covid-19/static/js/2.a5bc2c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7093bfc53d226cfd700c",
+    "url": "/react-covid-19/static/js/main.be4592c0.chunk.js"
+  },
+  {
+    "revision": "ee7abc1f3c3b00f69ae2",
+    "url": "/react-covid-19/static/js/runtime-main.cd811817.js"
+  }
+]);
